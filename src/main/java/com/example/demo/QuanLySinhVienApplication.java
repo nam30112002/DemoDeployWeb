fbf4fd10f48735005fa70c2d061b1a07902bba48
nam30112002 <nam30112002>
2023-10-30 21:16:19 +0700

    123

--- src/main/java/com/example/demo/QuanLySinhVienApplication.java
+++ src/main/java/com/example/demo/QuanLySinhVienApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class QuanLySinhVienApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuanLySinhVienApplication.class, args);
 	}
